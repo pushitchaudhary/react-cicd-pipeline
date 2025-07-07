fe9aef09d65b29d69676e40383e63b5ded19d28d
pushit chaudhary <pushitcoc@gmail.com>
2025-07-07 21:39:34 +0545

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <>
-      <p className='bg-red-500 text-5xl'>  This is cicd pipe line test wow</p>
+      <p className='bg-red-500 text-5xl'>  This is cicd pipe line test wow new</p>
     </>
   )
 }
